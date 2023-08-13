495474101f26b0b81572193f63deb7f38fc5aca6
Varun00786 <111782331+Varun00786@users.noreply.github.com>
2023-08-14 00:14:39 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,6 +1,7 @@
 import streamlit as st 
 import requests
 import joblib
+import pandas.core.indexes
 df=joblib.load('testF')
 tfidf_matrix=joblib.load('vectorF1')
 from sklearn.metrics.pairwise import cosine_similarity
